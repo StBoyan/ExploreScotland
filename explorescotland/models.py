5422055615a831cc95a6cd28b89429d58347f87c
Boyan Stoynov <28649002+StBoyan@users.noreply.github.com>
2018-02-28 15:12:03 +0000

    Initial database design created

--- explorescotland/models.py
+++ explorescotland/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Reduce max length of most attributes...
+class ParentUser(models.Model):
+    user = models.OneToOneField(User)
 
-# Create your models here.
+class Feedback(models.Model):
+    message = models.CharField(max_length=2000)
+    date = models.DateTimeField(default=datetime.date.today()) # may need to import something here
+    parent = models.ForeignKey(ParentUser)
+
+class ChildProfile(models.Model):           # Will probably need some attributes or more likely another model
+    parent = models.ForeignKey(ParentUser)  # to store the kids results for display in statistics (for parents)
+    name = models.CharField(max_length=128)
+    level = models.IntegerField(default=1)  # Assume kids start level 1. This field is probably wrong.. need to look into  how to access level
+
+class Level(models.Model):  # Need to find out how to access model ID
+    name = models.CharField(max_length=128, unique=True) # E.g. "Level 1"
+# Level and MaterialSubject may be optimised... need to consider this
+class MaterialSubject(models.Model):
+    name = models.CharField(max_length=128, unique=True) # E.g. "Lochs of Scotland"
+    level = models.ForeignKey(Level)
+
+class Material(models.Model):
+    name = models.CharField(max_length=128, unique=True) # E.g. "Loch Lomond" [attribute not in original ER]
+    content = models.CharField(max_length=2000) # E.g. "Loch Lomond is the biggest loch in Scotland..."
+    subject = models.ForeignKey(MaterialSubject)
+
+class QuizQuestion(models.Model):   # some sort of Questionid may be needed here
+    difficulty = models.IntegerField(default=0) #may need to drop this one
+    question = models.CharField(max_length=128, unique=True) # E.g. "What is the biggest loch in Scotland?"
+    CorrectAnswer = models.CharField(max_length=128) # E.g. "Loch Lomond"
+    incorrectAnswer1 = models.CharField(max_length=128) # E.g "Loch Ness"
+    incorrectAnswer2 = models.CharField(max_length=128) # E.g. "Loch Awe"
+    incorrectAnswer3 = models.CharField(max_length=128) # E.g. "Loch Katrine"
+    subject = models.ForeignKey(MaterialSubject)
