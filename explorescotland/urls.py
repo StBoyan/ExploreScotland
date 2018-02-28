8148462b2c523382a5b1a82e76b82bc03f275b3f
didi04 <2091499s@student.gla.ac.uk>
2018-02-28 15:54:02 +0000

    Scot, Lily, userHome views
    only basic views extending personaTemplate ... might need to reconsider urls :D

--- explorescotland/urls.py
+++ explorescotland/urls.py
@@ -3,4 +3,8 @@ from explorescotland import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^$', views.userHomePage, name='homePage'),
+    url(r'^$', views.scot, name='Scot'),
+    url(r'^$', views.lily, name='Lily'),
+
 ]
--- explorescotland/views.py
+++ explorescotland/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'explorescotland/home.html', {})
+
+def userHomePage (request):
+    return render(request, 'explorescotland/userHomePage.html', {})
+
+def scot (request):
+    return render(request, 'explorescotland/personaScot.html', {})
+
+def lily (request):
+    return render(request, 'explorescotland/personaLily.html', {})
