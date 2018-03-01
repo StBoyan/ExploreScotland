6437f429b69fdc6f7a54ae4283276cc2392a691b
Boyan Stoynov <28649002+StBoyan@users.noreply.github.com>
2018-03-01 19:12:44 +0000

    added persistent navigation bar, fixed uath, sample restricted page works

--- explorescotland/urls.py
+++ explorescotland/urls.py
@@ -3,6 +3,8 @@ from explorescotland import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^parent_area/', views.parent_area, name='parent_area'),
     url(r'^userHomePage/', views.userHomePage, name='homePage'),
     url(r'^personaScot/', views.scot, name='scot'),
     url(r'^personaLily/', views.lily, name='lily'),
--- explorescotland/views.py
+++ explorescotland/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'explorescotland/home.html', {})
 
+def about(request):
+    return render(request, 'explorescotland/about.html', {})
+
+@login_required
+def parent_area(request):
+    return render(request, 'explorescotland/parent_area.html', {})
+
 def parentlogin(request):
     return render(request,'explorescotland/parentlogin.html',{})
-    
+
 def userHomePage (request):
     return render(request, 'explorescotland/userHomePage.html', {})
 
