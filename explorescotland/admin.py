3d0fb3811e76532c241f7bc7f255353bbdaba071
Denivel8 <33163975+Denivel8@users.noreply.github.com>
2018-03-07 15:01:34 +0000

    added markers with the places and then this places are going to be clickable

--- explorescotland/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from explorescotland.models import ParentProfile, Feedback
-
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('message', 'date', 'parent')
-
-admin.site.register(Feedback, FeedbackAdmin)
-admin.site.register(ParentProfile)
