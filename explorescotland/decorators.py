e31c9405f675a2930639a3ce3ddf46557d0e7b1e
Boyan Stoynov <28649002+StBoyan@users.noreply.github.com>
2018-03-21 14:19:43 +0000

    children area displays message if no children

--- explorescotland/decorators.py
+++ explorescotland/decorators.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
+# Checks if child has been selected before proceeding
+# Redirects to children_area if not
 def child_is_selected(function):
     def wrap(request, *args, **kwargs):
+        # Checks if child_session has been created
         try:
             child_name = request.session['child_session']
         except KeyError:
             return HttpResponseRedirect(reverse('children_area'))
-
+        # Checks if child has "logged out"
         if child_name is None:
             return HttpResponseRedirect(reverse('children_area'))
 
